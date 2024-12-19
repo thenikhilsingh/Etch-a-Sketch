6cb5adda4a6ba217e2d6f6f8be8a250d292d588c
Nikhil Singh <nikhilsingh26042004@gmail.com>
2024-12-19 22:25:36 +0530

    added hover property to boxes

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
 let sketchContainer = document.querySelector(".sketchContainer");
 
 function grid() {
-  let boxes;
   for (let i = 0; i < 256; i++) {
-    boxes = document.createElement("div");
+    let boxes = document.createElement("div");
+    boxes.addEventListener("mouseover", () => {
+      boxes.style.cssText = "background:gray;";
+    });
     sketchContainer.append(boxes);
     boxes.className = "box";
   }
 }
 grid();
 
-
 function getRandomColor() {
-  let val1 = Math.ceil(0 + Math.random() * 255);//Math.ceil() is used to round off numbers
-  let val2 = Math.ceil(0 + Math.random() * 255);//Math.random() is used to generate random numbers
+  let val1 = Math.ceil(0 + Math.random() * 255);
+  let val2 = Math.ceil(0 + Math.random() * 255);
   let val3 = Math.ceil(0 + Math.random() * 255);
   return `rgb(${val1},${val2},${val3})`;
 }
-
-
-
-
