b1d61cf5cc35bec6ab313686abc1bae0b1ebff1a
Nikhil Singh <nikhilsingh26042004@gmail.com>
2024-12-20 00:06:42 +0530

    created Value button's working and fix some bugs

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 let sketchContainer = document.querySelector(".sketchContainer");
 
+let valueInput = 16;
+
 function grid() {
-  for (let i = 0; i < 256; i++) {
-    let boxes = document.createElement("div");
-    boxes.addEventListener("mouseover", () => {
-      boxes.style.cssText = "background:gray;";
-    });
-    sketchContainer.append(boxes);
-    boxes.className = "box";
+  for (let i = 0; i < valueInput; i++) {
+    let row = document.createElement("div");
+    row.className = "row";
+    for (let j = 0; j < valueInput; j++) {
+      let boxes = document.createElement("div");
+      boxes.addEventListener("mouseover", () => {
+        boxes.style.cssText = "background:gray;";
+      });
+      row.append(boxes);
+      boxes.className = "box";
+    }
+    sketchContainer.append(row);
   }
 }
 grid();
@@ -18,3 +25,13 @@ function getRandomColor() {
   let val3 = Math.ceil(0 + Math.random() * 255);
   return `rgb(${val1},${val2},${val3})`;
 }
+
+let ValueBtn = document.querySelector(".value");
+ValueBtn.addEventListener("click", () => {
+  let nodeList = document.querySelectorAll(".row");
+  for (let k = 0; k < valueInput; k++) {
+    nodeList[k].remove();
+  }
+  valueInput = prompt();
+  grid();
+});
